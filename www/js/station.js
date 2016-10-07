b44c9436bee3c2824e4f6db61da3019fc9f768e6
Dinuu <martin@zeilenwerk.ch>
2016-10-07 10:56:11 +0200

    Add active link function

--- www/js/station.js
+++ www/js/station.js
@@ -8,16 +8,23 @@ var app = {
     },
 
     onDeviceReady: function() {
-      // Wenn Netzwerk verbunden, prüfe Update
-      document.addEventListener('online', checkNetwork, false);
-
-      // QuerySelectors
-      var div_error = document.querySelector('.error');
-      var div_name = document.querySelector('.station-show-titel.list-box');
-      var items = document.querySelector('.items');
-
       // Parse the local storage
       var data = JSON.parse(localStorage.getItem("data"));
+
+      // Get station id from url param
+      var re = /[\?&]station_id=([0-9]+)/g;
+      var str = window.location.search;
+      var id = re.exec(str)[1];
+
+      // Suche Station anhand der ID in der JSON Datei
+      window.station = data.stations.filter(function(station) {
+        return station.id == id;
+      });
+
+      var source = $('.main').html();
+      var template = Handlebars.compile(source);
+      console.log(API_HOST);
+      $('.main').html(template({ host: API_HOST, guide: data, station: station[0] }));
 
       // Initialisiere Cache
       var initCache = function() {
@@ -40,114 +47,15 @@ var app = {
           });
       };
 
-      initCache();
+      setTimeout(initCache, 100);
 
-      checkNetwork();
+      displayData(data);
 
-      // Überprüfe Netzwerkstatus
-      function checkNetwork() {
-        console.log('checkNetwork function');
-        var networkState = navigator.connection.type;
-        if (networkState === Connection.NONE) {
-          console.log('Device is offline');
-        } else {
-          get(URL, checkUpdate, function(){});
-        }
-      }
+      function displayData(data) {
+        // Ermittle Beacons in der Nähe
+        startRangingBeacons(data);
 
-
-      // Prüft auf neue Updates und zeigt UpdateBox an
-      function checkUpdate(newData) {
-        console.log('checkUpdate function');
-        var cacheData = JSON.parse(localStorage.getItem("data"));
-        if (newData.updated_at !== cacheData.updated_at) {
-          var updateBox = document.querySelector('.update-box');
-          var p = document.querySelector('p');
-          var icon = document.querySelector('.close-icon');
-          updateBox.style.visibility = 'visible';
-          p.addEventListener('click', goUpdate);
-          icon.addEventListener('click', hideIcon);
-        }
-      }
-
-      // Wechsel zu Update Seite und anschliessend Update
-      function goUpdate() {
-        var updateBox = document.querySelector('.update-box');
-        updateBox.style.visibility = 'hidden';
-        window.location.replace('update.html');
-      }
-
-
-      // Ermittle Beacons in der Nähe
-      startRangingBeacons(data);
-
-
-      // Get station id from url param
-      var re = /[\?&]station_id=([0-9]+)/g;
-      var str = window.location.search;
-      var id = re.exec(str)[1];
-
-
-      // Suche Station anhand der ID in der JSON Datei
-      window.station = data.stations.filter(function(station) {
-        return station.id == id;
-      });
-
-
-      // Titel
-      var link_back = document.createElement('a');
-      link_back.setAttribute('href', 'index.html');
-
-      var icon = document.createElement('i');
-      icon.className += 'fa fa-chevron-left fa-left';
-      icon.setAttribute('aria-hidden', 'true');
-
-      var p_name = document.createElement('p');
-      p_name.innerHTML = station[0].titel;
-
-      link_back.appendChild(icon);
-      link_back.appendChild(p_name);
-      div_name.appendChild(link_back);
-
-
-      // Items in station
-      for (var i = 0; i < station[0].all_items.length; i++) {
-
-        if (station[0].all_items[i].kind === 'picture') {
-          var div = document.createElement('div');
-          div.className += 'item-image item';
-          var img = document.createElement('img');
-          img.setAttribute('src', API_HOST + station[0].all_items[i].file.url);
-          div.appendChild(img);
-
-        } else if (station[0].all_items[i].kind === 'audio') {
-          var div = document.createElement('div');
-          div.className += 'item-audio item';
-          var audio = document.createElement('audio');
-          audio.setAttribute('src', API_HOST + station[0].all_items[i].file.url);
-          audio.setAttribute('controls', '');
-          div.appendChild(audio);
-
-        } else if (station[0].all_items[i].kind === 'text') {1
-          var div = document.createElement('div');
-          div.className += 'item-text item';
-          var text = document.createElement('p');
-          text.innerHTML = station[0].all_items[i].text;
-          div.appendChild(text);
-
-        } else if (station[0].all_items[i].kind === 'video') {
-          var div = document.createElement('div');
-          div.className += 'item-video item';
-          var video = document.createElement('video');
-          video.setAttribute('src', API_HOST + station[0].all_items[i].file.url);
-          video.setAttribute('controls', '');
-          div.appendChild(video);
-
-        } else {
-
-        }
-
-        items.appendChild(div);
+        isActive();
 
         $('img,audio,video').each(function() {
             console.log('load cached files');
@@ -155,7 +63,8 @@ var app = {
         });
 
         getThumbnail();
-      };
+      }
+
 
       // Thumbnail für vorhandene Video
       function setPoster() {
@@ -174,9 +83,25 @@ var app = {
            video.load();
       }
 
+      function isActive() {
+        var re = /[\?&]station_id=([0-9]+)/g;
+        var str = window.location.search;
+        var id = re.exec(str)[1];
+
+        var links = document.querySelectorAll('.nav-links');
+        for (var i = 0; i < links.length; i++) {
+          var url = links[i].getAttribute('href');
+          var re = /[\?&]station_id=([0-9]+)/g;
+          var link_id = re.exec(url)[1];
+          if (id === link_id) {
+            links[i].parentNode.className += " active"
+          }
+        }
+      }
+
 
       function setTime() {
-        this.currentTime = 15;
+        this.currentTime = 20;
       }
 
 
@@ -188,16 +113,7 @@ var app = {
               videos[i].addEventListener('loadeddata', setPoster, false);
             }
       }
-
-      // Verstecke close icon
-      function hideIcon(e) {
-        e.stopPropagation();
-        var updateBox = document.querySelector('.update-box');
-        updateBox.style.visibility = 'hidden';
-      }
-
     },
-
 };
 
 app.initialize();
