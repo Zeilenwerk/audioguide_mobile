10711c6907610adcbe249d483b64dbd8653a63ec
Dinuu <martin@zeilenwerk.ch>
2016-10-14 16:27:58 +0200

    publish

--- platforms/android/assets/www/js/htmlparser.js
+++
@@ -1,5 +0,0 @@
-var HtmlParser = {
-  getImages: function(html) {
-    return $(html).find('img, audio, video').map(function() { return $(this).attr('src'); });
-  }
-};
--- platforms/android/assets/www/js/settings.js
+++
@@ -1,6 +0,0 @@
-var API_HOST = 'https://guide.zeilenwerk.ch';
-var API_GUIDE_URL = '/api/guides/3';
-var PUBLIC_GUIDE_URL = '/public/guides/3';
-var STATION_URL = API_HOST + PUBLIC_GUIDE_URL + '/stations/';
-var URL = API_HOST + API_GUIDE_URL;
-var PUBLIC_URL = API_HOST + PUBLIC_GUIDE_URL;
